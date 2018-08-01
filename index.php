28874733045f712b8623c5ca60f08d00f81d784e
MasamiGit <info@masami.main.jp>
2018-08-01 16:53:29 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>Pinterset</title>
+	<style>
+		body{
+			margin:0;
+			padding:0;
+		}
+		img{
+			display: block;
+			margin:0 auto;
+			width:100%;
+		}
+	</style>
+</head>
+<body>
+	<img src="img.jpg" alt="Pintersest">
+</body>
+</html>
